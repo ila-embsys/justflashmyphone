9450f25dfd0002c2d5c13afc55265361974a7d91
ila <ila.embsys@gmail.com>
2025-04-06 18:29:02 +0000

    test dist

---
+++ assets/allPathsLoader-D9jEV_ZY.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-CFYfzcNL.js","assets/index-Dbplf1cB.js","assets/index-DBZ0NyaA.js","assets/index-DEiNh4-i.js","assets/index-ScS1Va63.css"])))=>i.map(i=>d[i]);
+import{_ as o,a as n,b as i}from"./index-DEiNh4-i.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-CFYfzcNL.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
