b1efb408f2206820d2dd3b1bd3257f5d1297c2af
ila <ila.embsys@gmail.com>
2025-05-25 08:41:47 +0000

    Update

--- assets/allPaths-CFYfzcNL.js
+++
@@ -1,1 +0,0 @@
-import{I as n}from"./index-Dbplf1cB.js";import{I as e}from"./index-DBZ0NyaA.js";import{p as r,I as s}from"./index-DEiNh4-i.js";function I(o,t){var a=r(o);return t===s.STANDARD?n[a]:e[a]}function p(o){return r(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,I as getIconPaths,p as iconNameToPathsRecordKey};
