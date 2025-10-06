55a7b2ade9ac48e2a601cc65bbb31d170f95420c
ila <ila.embsys@gmail.com>
2025-10-06 06:16:05 +0400

    Apply the new approach of emscripten calling

--- src/Emmcdl.tsx
+++ src/Emmcdl.tsx
@@ -1,30 +1,8 @@
 // ./emmcdl dir is artifact of building
-import EmscriptenModuleLoader from "./emmcdl/emmcdl";
+import * as EmscriptenModuleLoader from "./emmcdl/emmcdl";
 
-declare global {
-  interface Navigator {
-    // This USB type comes from @types/w3c-web-usb
-    readonly usb: USB;
-  }
-}
-
-function main(args: string[]) {
-  console.log("crossOriginIsolated: ", crossOriginIsolated);
-  // Commented out to not bother the user since
-  // this variant of flashing doesn't work
-  // await navigator.usb.requestDevice({
-  //   filters: [{ vendorId: 0x05c6, productId: 0x9008 }],
-  // });
-
-  EmscriptenModuleLoader({
-    arguments: args,
-    print: (text: string) => {
-      console.log("stdout: " + text);
-    },
-    printErr: (text: string) => {
-      console.warn("stderr: " + text);
-    },
-  });
+function main(options: any) {
+  return (EmscriptenModuleLoader as any).default(options);
 }
 
 export default main;
